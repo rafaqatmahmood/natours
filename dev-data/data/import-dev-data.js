288e8a08faa8d274731d253954d77adb48a19bc6
Rafaqat Mahmood <rafaqatmahmood81@gmail.com>
2024-06-17 20:41:58 +0500

    Script for importing and deleting tours

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({
+  path: `${__dirname}/../../.env`,
+});
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successfull!'));
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+// IMPORT DATA TO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  } finally {
+    process.exit();
+  }
+};
+
+// DELETE DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  } finally {
+    process.exit();
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
