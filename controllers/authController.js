e8e3838f1ecc1c83bf669d4730674de843129aac
Rafaqat Mahmood <rafaqatmahmood81@gmail.com>
2024-08-31 11:01:48 +0500

    Sending JWT via Cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,18 @@ const createSendToken = (user, res, statusCode) => {
   // Log the user in, send JWT
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    secure: false,
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
   res.status(statusCode).json({ status: 'success', token });
 };
 
